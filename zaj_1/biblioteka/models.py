756989f1574a7e929f279e81e705ad2400c3ec09
gosiazagorska <mzagorska4@st.swps.edu.pl>
2025-11-07 13:35:41 +0100

    zajecia.7.11

--- zaj_1/biblioteka/models.py
+++ zaj_1/biblioteka/models.py
@@ -10,6 +10,11 @@ MONTHS = models.IntegerChoices(
     'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień'
 )
 
+PLCIE = models.IntegerChoices(
+    'PLEC',
+    'Kobieta Mezczyzna Inna'
+)
+
 # Lista wyboru formatu książki
 BOOK_FORMATS = (
     ('P', 'Papierowa'),
@@ -68,8 +73,9 @@ class Osoba(models.Model):
     )
     imie = models.CharField(max_length = 50, null = False, blank = False)
     nazwisko = models.CharField(max_length = 100, null = False, blank = False)
-    plec = models.CharField(max_length=1, choices = PLEC_WYBOR, default = 'I')
+    plec = models.IntegerField(choices = PLCIE.choices, default = PLCIE.choices[2][0])
     stanowisko = models.ForeignKey('Stanowisko', on_delete = models.CASCADE)
+    data_dodania = models.DateField(auto_now_add = True, editable = False)
 
 class Stanowisko(models.Model):
     nazwa = models.CharField(max_length = 70, null = False, blank = False)
